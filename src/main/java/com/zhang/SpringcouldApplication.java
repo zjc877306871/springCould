c03cb193d9db61301de405a404d45f99b23ecfa4
zhangjianchong <1204880901@qq.com>
2018-01-22 10:00:06 +0800

    Initial commit

---
+++ src/main/java/com/zhang/SpringcouldApplication.java
@@ -0,0 +1,12 @@
+package com.zhang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcouldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcouldApplication.class, args);
+	}
+}
