429a9f030213378bedf3a3b132ca80f77abba5b7
zhangjianchong <1204880901@qq.com>
2018-01-22 10:05:12 +0800

    服务方子模块

---
+++ springprovider/src/main/java/com/zhang/SpringproviderApplication.java
@@ -0,0 +1,12 @@
+package com.zhang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringproviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringproviderApplication.class, args);
+	}
+}
