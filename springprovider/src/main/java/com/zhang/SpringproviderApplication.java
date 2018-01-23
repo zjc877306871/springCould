b160594c0271653e17e310216c207e13d016056c
zhangjianchong <1204880901@qq.com>
2018-01-23 17:31:20 +0800

    eureka  客户端配置优化

--- springprovider/src/main/java/com/zhang/SpringproviderApplication.java
+++ springprovider/src/main/java/com/zhang/SpringproviderApplication.java
@@ -2,11 +2,11 @@ package com.zhang;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableAutoConfiguration
 @SpringBootApplication
+@EnableDiscoveryClient
 @MapperScan(basePackages = "com.zhang.mapper")
 public class SpringproviderApplication {
 
--- springprovider/src/main/java/com/zhang/controller/QueryUserInfo.java
+++ springprovider/src/main/java/com/zhang/controller/QueryUserInfo.java
@@ -1,5 +1,7 @@
 package com.zhang.controller;
 
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
 import com.zhang.entity.User;
 import com.zhang.service.UserInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,15 +15,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class QueryUserInfo {
 
-
     @Autowired
     private UserInfoService userInfoService;
 
+    @Autowired(required = false)
+    private EurekaClient clientlient;
+
     @GetMapping("/user/{id}")
     public User queryUserInfo(@PathVariable String id){
-
         User user =  userInfoService.queryUserInfo(id);
         System.out.println("userId="+user.getUserId());
         return user;
+    }
+
+    @GetMapping("/eureka")
+    public String serviceUrl() {
+        InstanceInfo instance = clientlient.getNextServerFromEureka("springprovider", false);
+        return instance.getHomePageUrl();
     }
 }
