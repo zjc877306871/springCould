646953141ec283d6bf82ac4fb5011ba16690adce
zhangjianchong <1204880901@qq.com>
2018-01-24 17:38:20 +0800

    集合ribbon  实现负载均衡1

--- springconsumer/src/main/java/com/zhang/controller/HelloController.java
+++ springconsumer/src/main/java/com/zhang/controller/HelloController.java
@@ -2,6 +2,7 @@ package com.zhang.controller;
 
 import com.zhang.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -16,8 +17,9 @@ public class HelloController {
     @Autowired
     private RestTemplate restTemplate;
 
+    @GetMapping("/user/{id}")
     public User queryUserInfo(@PathVariable String id){
-        User user = restTemplate.getForObject("",User.class);
+        User user = restTemplate.getForObject("http://springprovider/user/"+id,User.class);
         return user;
     }
 
