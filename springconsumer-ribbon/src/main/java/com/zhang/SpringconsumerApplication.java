8401f1e74df6c6bbaef634de075327d384ff5948
zhangjianchong <1204880901@qq.com>
2018-01-30 11:09:28 +0800

    添加子模块--通过配置文件实现riboon配置

--- springconsumer-ribbon/src/main/java/com/zhang/SpringconsumerApplication.java
+++ springconsumer-ribbon/src/main/java/com/zhang/SpringconsumerApplication.java
@@ -1,24 +1,17 @@
 package com.zhang;
 
 
-import com.zhang.config.ExcludeFromComponentScan;
-import com.zhang.config.TestRibbonConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
 import org.springframework.web.client.RestTemplate;
 
 @EnableAutoConfiguration
 @SpringBootApplication
 @EnableEurekaClient
-@RibbonClient(name = "springprovider1", configuration = TestRibbonConfig.class)
-@ComponentScan(excludeFilters = {@ComponentScan.Filter(type = FilterType.ANNOTATION, value = ExcludeFromComponentScan.class)})
 public class SpringconsumerApplication {
 
 	//实现ribbon负载均衡
